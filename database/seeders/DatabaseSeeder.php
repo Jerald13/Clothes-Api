5d9c454adcbe2fda4a5240a103e5318e9c8c7e05
JrealFun <jeraldlee2002@gmail.com>
2023-05-05 20:40:01 +0800

    seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            VoucherSeeder::class,
+            BankSeeder::class,
+            BankAccountSeeder::class,
+            DeliverySeeder::class,
+            FreeGiftSeeder::class,
+            LogisticSeeder::class,
+            UsersSeeder::class,
+            VoucherSeeder::class,
+        ]);
     }
 }
